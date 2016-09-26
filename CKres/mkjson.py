5c19f3efee2cf41b185c407a180fb305bec14313
colding <jsgydcy@163.com>
2016-09-26 23:55:53 +0800

    添加创建文件夹功能

--- CKres/mkjson.py
+++ CKres/mkjson.py
@@ -2,18 +2,46 @@
 # -*- coding:utf-8 -*-
 import glob
 import os
+import shutil
 
 def creat_web_dir():
-    path = os.path.abspath("/android")
+    path = os.path.abspath("android")
     print path
-    file_name = glob.glob("./android/")
+    list_name = glob.glob('*.xmpf')
+    file_name = list_name[0]
     print file_name
 
-def creat_file_dir():
-    if os.path.isdir('./result'):
+    file = open(file_name, 'rt')
+    line_file = file.readlines()
+    site_name = ""
+    for line in line_file:
+        if line.find("SiteName") != -1:
+            site_name = str(line)[12:len(line) -1].decode(encoding='utf-8')
+            # site = site_name.decode(encoding='utf-8')
+            if os.path.isdir(site_name):
+                pass
+            else:
+                os.mkdir(site_name)
+
+    cap_name = glob.glob('*.cap')
+    print cap_name
+    for cap in cap_name:
+        shutil.copy(cap, site_name)
+
+    if os.path.isdir(site_name + '/result'):
         pass
     else:
-        os.mkdir('result')
+        os.makedirs(site_name + '/result/')
+
+    list_cap = glob.glob('*.cap')
+    print list_cap
+
+    shutil.rmtree(site_name + '/result')
+    for list in list_cap:
+        # print list
+        list_name = list[:-4]
+        os.makedirs(site_name + '/result/' + list_name)
+
 
 if __name__ == '__main__':
     creat_web_dir()
